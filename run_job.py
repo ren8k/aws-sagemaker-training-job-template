bd020c9a265aceb1d233353f452cde9df966ce91
Renya.Kujirada <rsmilek5492@gmail.com>
2024-03-17 11:08:30 +0000

    Refactor Experiment class and add timestamp and experiment name

--- run_job.py
+++ run_job.py
@@ -10,17 +10,27 @@ import src.utils
 DATASET_S3_URI = "s3://sm-train-1710652103"
 REGION = "ap-northeast-1"
 INSTANCE_TYPE = "ml.g4dn.xlarge"
-JOB_NAME = "mnist"
+TIMESTAMP = src.utils.get_timestamp()
+EXP_NAME = "mnist"
 
 
 class Experiment:
     def __init__(self, args) -> None:
-        os.environ["AWS_DEFAULT_REGION"] = REGION
+        self.region = REGION
+        os.environ["AWS_DEFAULT_REGION"] = self.region
         self.session = sagemaker.Session()
         self.role = sagemaker.get_execution_role()
         self.hp = src.utils.load_config(args.config)
         self.instance_type = INSTANCE_TYPE
-        self.job_name = f"{JOB_NAME}-{os.path.basename(args.config).split('.')[0]}"
+        self.conf_name = os.path.basename(args.config).split(".")[0]
+
+        self.exp_name = EXP_NAME
+        self.job_name = f"{self.exp_name}-{self.conf_name}-{TIMESTAMP}"
+        self.run_name = f"run-{TIMESTAMP}"
+
+        # add sm exp settings to hyperparameters
+        self.hp["experiment-name"] = self.exp_name
+        self.hp["run-name"] = self.run_name
 
     def run(self):
         estimator = PyTorch(
@@ -30,10 +40,10 @@ class Experiment:
             framework_version="2.0.0",
             py_version="py310",
             instance_count=1,
-            instance_type=INSTANCE_TYPE,
+            instance_type=self.instance_type,
             hyperparameters=self.hp,
             base_job_name=self.job_name,
-            # environment={"AWS_DEFAULT_REGION": "ap-nottheast-1"},
+            environment={"AWS_DEFAULT_REGION": self.region},
             keep_alive_period_in_seconds=1800,
         )
 
@@ -50,7 +60,12 @@ def get_args():
 
 def main(args):
     exp = Experiment(args)
-    exp.run()
+    with Run(
+        experiment_name=exp.exp_name,
+        sagemaker_session=exp.session,
+        run_name=exp.run_name,
+    ) as run:
+        exp.run()
 
 
 if __name__ == "__main__":
