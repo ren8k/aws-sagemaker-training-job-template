b8eeee1759bcb8bd01f68c158b8971ba0cfc2e8f
Renya.Kujirada <rsmilek5492@gmail.com>
2024-03-17 07:06:38 +0000

    Add statement to cd current dir

--- src/train.sh
+++ src/train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+cd "$(dirname "$0")"
 export SM_CHANNEL_TRAINING="/app/sm-train/dataset"
 export SM_OUTPUT_DATA_DIR="/app/sm-train/output/metrics"
 export SM_MODEL_DIR="/app/sm-train/output/model"
