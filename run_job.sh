731c5abd0eb292330f353bce6e9990a9eec62f0d
Renya.Kujirada <rsmilek5492@gmail.com>
2024-03-17 13:00:55 +0000

    Update run_job.sh to use src directory for run_job.py

--- run_job.sh
+++ run_job.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")"
+
 EXP_ID=$1 # three digits number for experiment id
 CONF_PATH=/app/sm-train/config/exp$EXP_ID.yaml
 
-python run_job.py --config $CONF_PATH
+python src/run_job.py --config $CONF_PATH
