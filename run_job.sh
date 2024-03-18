03c595a621a7180b640a69e7ff5cbeaeeacaa8ae
Renya.Kujirada <rsmilek5492@gmail.com>
2024-03-18 15:22:54 +0000

    Add output directory to run_job.sh

--- run_job.sh
+++ run_job.sh
@@ -8,9 +8,11 @@ DATASET_S3_URI=s3://sm-train-1710652103
 EXP_NAME=mnist
 INSTANCE_TYPE=ml.g4dn.xlarge
 REGION=ap-northeast-1
+OUT_DIR="./output/model"
 
 python src/run_job.py --config $CONF_PATH \
     --dataset-uri $DATASET_S3_URI \
     --exp-name $EXP_NAME \
     --instance-type $INSTANCE_TYPE \
-    --region $REGION
+    --region $REGION \
+    --out-dir $OUT_DIR
