75a2c34c393cebfbc66c01b0d128c1aeecc83ff6
Renya.Kujirada <rsmilek5492@gmail.com>
2024-03-23 09:12:16 +0000

    Delete dataset upload script

--- dataset/upload_dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-BUCKETNAME=sm-train-$(date +%s)
-REGION=ap-northeast-1
-DATA_DIR=../dataset
-
-aws s3api create-bucket --bucket $BUCKETNAME --region $REGION \
-    --create-bucket-configuration LocationConstraint=$REGION
-aws s3 cp $DATA_DIR s3://$BUCKETNAME --recursive
