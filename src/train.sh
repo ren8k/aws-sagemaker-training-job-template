b06ab8aed098facb5cc2f9285c54805f1777490b
Renya.Kujirada <rsmilek5492@gmail.com>
2024-03-23 07:05:45 +0000

    Add save_csv function to save metrics in a CSV file

--- src/train.sh
+++ src/train.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+
 export SM_CHANNEL_TRAINING="/app/sm-train/dataset"
-export SM_OUTPUT_DATA_DIR="/app/sm-train/output/record"
-export SM_MODEL_DIR="/app/sm-train/output/model"
+export SM_OUTPUT_DATA_DIR="/app/sm-train/result/output"
+export SM_MODEL_DIR="/app/sm-train/result/model"
 
 python train.py
